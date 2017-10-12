3e3d98634347725370066cbcc7745d1fb273faca
Derek Hendrickx <derek@hendrickx.me>
2017-10-11 21:30:58 -0400

    Add PlayerController

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody m_rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		m_rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/// <summary>
+	/// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void FixedUpdate()
+	{
+		var moveHorizontal = Input.GetAxis("Horizontal");
+		var moveVertical = Input.GetAxis("Vertical");
+		var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		m_rigidbody.AddForce(movement);
+	}
+}
